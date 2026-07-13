1c845b3181de4f2f8d55efdf5dc2fbff25873790
Andre Stubbe <andrestubbe@proton.me>
2026-07-13 11:03:39 +0200

    Move generated UtilityClass.java to correct src directory

--- examples/Demo/UtilityClass.java
+++
@@ -1,2 +0,0 @@
-public class UtilityClass { \n public static void main(String[] args) {\n // Create methods here for getting current date, tomorrow's date, and yesterday's date\
- }\n}
---
+++ examples/Demo/src/main/java/demo/UtilityClass.java
@@ -0,0 +1,7 @@
+package demo;
+
+public class UtilityClass { 
+    public static void main(String[] args) {
+        // Create methods here for getting current date, tomorrow's date, and yesterday's date
+    }
+}
